30f483ac19b20ef0c9e015f2e95d5f93ea56458f
matteoxx <mateuszniziolek96@gmail.com>
2019-07-04 13:29:35 +0200

    initial commit

---
+++ android/app/src/main/java/com/catering/MainActivity.java
@@ -0,0 +1,13 @@
+package com.catering;
+
+import com.reactnativenavigation.NavigationActivity;
+import android.os.Bundle; 
+import com.facebook.react.ReactActivity;
+
+
+public class MainActivity extends NavigationActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
