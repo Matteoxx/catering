82fc0a3eb6fe24da02eee7a05219d252a9671efc
matteoxx <mateuszniziolek96@gmail.com>
2019-07-04 15:10:22 +0200

    auth

--- src/screens/Initializing.js
+++ src/screens/Initializing.js
@@ -1,12 +1,44 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
+
+import { goToAuth, goHome } from '../navigation';
+
+import { USER_KEY } from '../config';
 
 export default class Initializing extends Component {
+
+  async componentDidMount() {
+    try {
+      const user = await AsyncStorage.getItem(USER_KEY)
+      console.log('user: ', user)
+      if (user) {
+        goHome()
+      } else {
+        goToAuth()
+      }
+    } catch (err) {
+      console.log('error: ', err)
+      goToAuth()
+    }
+  }
+
     render() {
       return (
-        <View>
-          <Text>Loading</Text>
+        <View style={styles.container}>
+          <Text style={styles.text}>Loading</Text>
         </View>
       )
     }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  text: {
+    fontSize: 28
+  },
+})
--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -1,12 +1,82 @@
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import React from 'react';
+import {
+  View,
+  Button,
+  Text,
+  TextInput,
+  StyleSheet
+} from 'react-native';
 
-export default class SignUp extends Component {
-    render() {
-      return (
-        <View>
-          <Text>SignUp</Text>
-        </View>
-      )
+export default class SignUp extends React.Component {
+  state = {
+    username: '', password: '', email: '', phone_number: ''
+  }
+  onChangeText = (key, val) => {
+    this.setState({ [key]: val })
+  }
+  signUp = async () => {
+    const { username, password, email, phone_number } = this.state
+    try {
+      // here place your signup logic
+      console.log('user successfully signed up!: ', success)
+    } catch (err) {
+      console.log('error signing up: ', err)
     }
+  }
+ 
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Rejestracja</Text>
+        <TextInput
+          style={styles.input}
+          placeholder='Username'
+          autoCapitalize="none"
+          onChangeText={val => this.onChangeText('username', val)}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder='Password'
+          secureTextEntry={true}
+          autoCapitalize="none"
+          onChangeText={val => this.onChangeText('password', val)}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder='Email'
+          autoCapitalize="none"
+          onChangeText={val => this.onChangeText('email', val)}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder='Phone Number'
+          autoCapitalize="none"
+          onChangeText={val => this.onChangeText('phone_number', val)}
+        />
+        <Button
+          title='Załóż konto'
+          onPress={this.signUp}
+        />
+      </View>
+    )
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  input: {
+    width: 350,
+    fontSize: 18,
+    height: 55,
+    borderColor: '#42A5F5',
+    borderWidth: 1,
+    margin: 10,
+    padding: 8,
+    borderRadius: 14
+  }
+
+})
